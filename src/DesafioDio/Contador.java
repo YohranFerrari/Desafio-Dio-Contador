9286f57964b2b1962a3305fdf316dc1657f69ce2
yohran@outlook.com <yohran@outlook.com>
2022-08-24 16:43:12 -0300

    first commit

---
+++ src/DesafioDio/Contador.java
@@ -0,0 +1,30 @@
+package DesafioDio;
+import java.util.Scanner;
+
+public class Contador {
+	public static void main(String[] args) {
+
+		Scanner terminal = new Scanner(System.in);
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = terminal.nextInt();
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = terminal.nextInt();
+
+		try {
+			contar(parametroUm, parametroDois);
+		} catch (ParametrosInvalidosException e) {
+			System.err.println("O segundo parâmetro deve ser maior que o primeiro");
+
+		}
+		terminal.close();
+	}
+
+	static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+		if (parametroUm > parametroDois)
+			throw new ParametrosInvalidosException();
+
+		int contagem = parametroDois - parametroUm;
+		System.out.println(contagem);
+
+	}
+}
